e10485295eec9ec1b32372dac527c8e1a82d88f4
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-09-15 16:58:03 -0400

    add plane

--- objects.py
+++ objects.py
@@ -33,6 +33,33 @@ def add_object(model_path, scale = 1, trans_vec = (0, 0, 0), rot_mat = ((1, 0, 0
     return objs
 
 
+def add_plane(scale = 1, trans_vec = (0, 0, 0), rot_mat = ((1, 0, 0), (0, 1, 0), (0, 0, 1)), name = None):
+    scene = bpy.context.scene
+
+    # add plane
+    bpy.ops.mesh.primitive_plane_add(location = (0, 0, 0))
+    obj = scene.objects.active
+
+    # rename object
+    if name is not None:
+        obj.name = name
+
+    # compute world matrix
+    trans_4x4 = Matrix.Translation(trans_vec)
+    rot_4x4 = Matrix(rot_mat).to_4x4()
+    scale_4x4 = Matrix(np.eye(4))
+    obj.matrix_world = trans_4x4 * rot_4x4 * scale_4x4
+
+    # scale
+    obj.scale = (scale, scale, scale)
+
+    # update scene
+    bpy.context.scene.update()
+
+    return obj
+
+
+
 def set_material(obj, material):
     scene = bpy.context.scene
     scene.objects.active = obj
