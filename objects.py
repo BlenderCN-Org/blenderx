6f3da7dfd03b26a002b4769b4c87f8939ced879a
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-09-15 12:47:17 -0400

    clean up

--- objects.py
+++ objects.py
@@ -3,24 +3,33 @@ import numpy as np
 from mathutils import Matrix
 
 def add_object(model_path, rot_mat = ((1, 0, 0), (0, 1, 0), (0, 0, 1)), trans_vec = (0, 0, 0), scale = 1, name = None):
+    # import object
     bpy.ops.import_scene.obj(filepath = model_path)
 
-    obj_list = []
-    for i, obj in enumerate(bpy.context.selected_objects):
+    objs = []
+    for k, obj in enumerate(bpy.context.selected_objects):
+        # rename objects
         if name is not None:
             if len(bpy.context.selected_objects) == 1:
                 obj.name = name
             else:
-                obj.name = name + '-' + str(i)
+                obj.name = name + '-' + str(k + 1)
 
+        # compute world matrix
         trans_4x4 = Matrix.Translation(trans_vec)
         rot_4x4 = Matrix(rot_mat).to_4x4()
         scale_4x4 = Matrix(np.eye(4))
         obj.matrix_world = trans_4x4 * rot_4x4 * scale_4x4
 
+        # scale
         obj.scale = (scale, scale, scale)
-        obj_list.append(obj)
-    return obj_list
+
+        objs.append(obj)
+
+    # update scene
+    bpy.context.scene.update()
+
+    return objs
 
 
 def set_material(obj, material):
