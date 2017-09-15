aed2332fbad8a43a6f60f05166af0d1d0539b19f
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-09-15 17:40:04 -0400

    bug exists: weird physics behavior

--- physics.py
+++ physics.py
@@ -2,13 +2,13 @@ import bpy
 import numpy as np
 
 
-def set_physics(obj, physics_type = 'RIGID_BODY', collision_type = None, mass = 1.):
+def set_physics(obj, physics_type = 'RIGID_BODY', collision_type = None, compound = False, mass = 1):
     obj.game.physics_type = physics_type
     if collision_type is not None:
         obj.game.use_collision_bounds = True
         obj.game.collision_bounds_type = collision_type
-        obj.game.collision_margin = 0.
-        obj.game.use_collision_compound = True
+        obj.game.collision_margin = 0
+        obj.game.use_collision_compound = compound
     obj.game.mass = mass
 
 
