c350cd56bb6a4a19a22d9f9756eb7ac02ddc15b0
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-10-22 00:27:46 -0400

    remove redundancies

--- __init__.py
+++ __init__.py
@@ -1,3 +1,2 @@
-from .constraints import fix_objects
 from .objects import add_object, add_plane, add_sphere, join_objects, set_material
 from .scene import render_animation
--- objects.py
+++ objects.py
@@ -7,39 +7,25 @@ from .scene import deselect_all_objects
 
 def add_object(model_path, scale = 1, trans_vec = (0, 0, 0), rot_mat = ((1, 0, 0), (0, 1, 0), (0, 0, 1)), name = None):
     # import the object
-    if model_path.endswith('.obj'):
-        bpy.ops.import_scene.obj(filepath = model_path)
-    elif model_path.endswith('.dae'):
-        bpy.ops.wm.collada_import(filepath = model_path)
-
-    # deselect all objects
-    deselect_all_objects()
+    bpy.ops.import_scene.obj(filepath = model_path)
 
     objs = []
     for k, obj in enumerate(bpy.context.selected_objects):
-        if obj.type == 'MESH':
-            obj.select = False
+        # rename the objects
+        if name is not None:
+            if len(bpy.context.selected_objects) == 1:
+                obj.name = name
+            else:
+                obj.name = name + '-' + str(k + 1)
 
-            # rename the objects
-            if name is not None:
-                if len(bpy.context.selected_objects) == 1:
-                    obj.name = name
-                else:
-                    obj.name = name + '-' + str(k + 1)
+        # compute the world matrix
+        trans_4x4 = Matrix.Translation(trans_vec)
+        rot_4x4 = Matrix(rot_mat).to_4x4()
+        obj.matrix_world = trans_4x4 * rot_4x4
 
-            # compute the world matrix
-            trans_4x4 = Matrix.Translation(trans_vec)
-            rot_4x4 = Matrix(rot_mat).to_4x4()
-            obj.matrix_world = trans_4x4 * rot_4x4
-
-            # scale
-            obj.scale = (scale, scale, scale)
-            objs.append(obj)
-        else:
-            obj.select = True
-
-    # remove all non-mesh objects
-    bpy.ops.object.delete()
+        # scale
+        obj.scale = (scale, scale, scale)
+        objs.append(obj)
 
     # update the scene
     bpy.context.scene.update()
