56c0ee701096730d1f7e5a5fae2ce8624cf59768
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-10-24 20:28:25 -0400

    edits (render depth; with bugs)

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 from .objects import add_object, add_plane, add_sphere, clean_object, join_objects, remesh_object, separate_object
-from .scene import render_animation
+from .scene import render_animation, render_depth
